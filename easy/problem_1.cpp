8287f1a0b03b40986b737a61e2a6b6f7fb0724cd
tatevsrapivonyan <tatev.srapivonyan@gmail.com>
2024-06-10 17:32:00 +0400

    Problem 1

---
+++ easy/problem_1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (m.find(target - nums[i]) == m.end())
+            {
+                m[nums[i]] = i;
+            }
+            else
+            {
+                return {m.find(target - nums[i])->second, i};
+            }
+        }
+        return {};
+    }
+};
