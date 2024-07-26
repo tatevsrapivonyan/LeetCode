f3603267f57e145fb8f23f1a677aa1980d844425
tatevsrapivonyan <tatev.srapivonyan@gmail.com>
2024-07-26 15:07:12 +0400

    #13

--- README.md
+++ README.md
@@ -16,4 +16,5 @@ Problems solved:
 
 #1 [Two Sum](https://leetcode.com/problems/two-sum/description/) <br>
 #7 [Reverse Integer](https://leetcode.com/problems/reverse-integer/description/) <br>
-#9 [Palindrome Number](https://leetcode.com/problems/palindrome-number/description/) <br> 
+#9 [Palindrome Number](https://leetcode.com/problems/palindrome-number/description/) <br>
+#13 [Roman to Integer](https://leetcode.com/problems/roman-to-integer/description/) <br>
---
+++ easy/problem_13.cpp
@@ -0,0 +1,114 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int s_to_num {};
+        size_t i = 0;
+        for (; i < s.size() - 1; ++i)
+        {
+            if (s[i] =='I')
+            {
+                if (s[i + 1] == 'V')
+                {
+                    s_to_num += 4;
+                    ++i;
+                    continue;
+                }
+                if (s[i + 1] == 'X')
+                {
+                    s_to_num += 9;
+                    ++i;
+                    continue;
+                }
+                s_to_num += 1;
+            }
+            if (s[i] == 'X')
+            {
+                if (s[i + 1] == 'L')
+                {
+                    s_to_num += 40;
+                    ++i;
+                    continue;
+                }
+                if (s[i + 1] == 'C')
+                {
+                    s_to_num += 90;
+                    ++i;
+                    continue;
+                }
+                s_to_num += 10;
+            }
+            if (s[i] == 'C')
+            {
+                if (s[i + 1] == 'D')
+                {
+                    s_to_num += 400;
+                    ++i;
+                    continue;
+                }
+                if (s[i + 1] == 'M')
+                {
+                    s_to_num += 900;
+                    ++i;
+                    continue;
+                }
+                s_to_num += 100;
+            }
+            if (s[i] == 'V')
+            {
+                s_to_num += 5;
+            }
+            if (s[i] == 'L')
+            {
+                s_to_num += 50;
+            }
+            if (s[i] == 'D')
+            {
+                s_to_num += 500;
+            }
+            if (s[i] == 'M')
+            {
+                s_to_num += 1000;
+            }
+        }
+        if (s[i] == 'I')
+        {
+            s_to_num += 1;
+        }
+
+        if (s[i] == 'V')
+        {
+            s_to_num += 5;
+        }
+
+        if (s[i] == 'X')
+        {
+            s_to_num += 10;
+        }
+
+        if (s[i] == 'L')
+        {
+            s_to_num += 50;
+        }
+
+        if (s[i] == 'C')
+        {
+            s_to_num += 100;
+        }
+
+        if (s[i] == 'D')
+        {
+            s_to_num += 500;
+        }
+
+        if (s[i] == 'M')
+        {
+            s_to_num += 1000;
+        }
+
+        return s_to_num;
+    }
+};
