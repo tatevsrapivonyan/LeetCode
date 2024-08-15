f82bc2c44302179916ef2879438f1c62e9688f8c
Tatev <tatev.srapivonyan@gmail.com>
2024-08-15 23:14:36 +0400

    1476

--- README.md
+++ README.md
@@ -41,3 +41,4 @@ Problems solved:
 #2798 [Number of Employees Who Met the Target](https://leetcode.com/problems/number-of-employees-who-met-the-target/description/)<br>
 #1431 [Kids With the Greatest Number of Candies](https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/)<br>
 #2824 [Count Pairs Whose Sum is Less than Target](https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/)<br>
+#1476 [Subrectangle Queries](https://leetcode.com/problems/subrectangle-queries/description/)<br>
---
+++ medium/problem_1476.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+class SubrectangleQueries {
+    vector<vector<int>> rectangle;
+public:
+    SubrectangleQueries(vector<vector<int>>& rectangl) {
+        rectangle = rectangl;
+    }
+
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (size_t i = row1; i <= row2; ++i)
+        {
+            for (size_t j = col1; j <= col2; ++j)
+            {
+                rectangle[i][j] = newValue;
+            }
+        }
+    }
+
+    int getValue(int row, int col) {
+        return rectangle[row][col];
+    }
+};
