fc162fbc54fadde4ce98e8f3275e28006d67c63b
Tatev <tatev.srapivonyan@gmail.com>
2024-09-08 22:23:24 +0400

    1551 version 2

--- medium/problem_1551.cpp
+++ medium/problem_1551.cpp
@@ -10,3 +10,17 @@ public:
         return answer;
     }
 };
+
+//Version 2 
+
+class Solution {
+public:
+    int minOperations(int n) {
+        if (n % 2 == 0)
+        {
+            return n / 2 * n / 2;
+        }
+
+        return (n - 1) / 2 * (n + 1) / 2;
+    }
+};
