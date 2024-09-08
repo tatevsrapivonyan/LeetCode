22b60d7ff491045c505d793cf0625b244556574b
Tatev <tatev.srapivonyan@gmail.com>
2024-09-08 21:32:04 +0400

    66 optimization

--- easy/problem_61.cpp
+++ easy/problem_61.cpp
@@ -5,32 +5,20 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for (int i = digits.size() - 1; i >= 0; --i)
+        int j = digits.size() - 1;
+        while (j >= 0 && digits[j] == 9)
         {
-            if (digits[i] < 9)
-            {
-                digits[i] += 1;
-                return digits;
-            }
-            else
-            {
-                int j{ i };
-                while (j >= 0 && digits[j] == 9)
-                {
-                    digits[j] = 0;
-                    --j;
-                }
-                if (j < 0)
-                {
-                    digits[j + 1] = 1;
-                    digits.push_back(0);
-                }
-                else
-                {
-                    digits[j] += 1;
-                }
-                return digits;
-            }
+            digits[j] = 0;
+            --j;
+        }
+        if (j < 0)
+        {
+            digits[j + 1] = 1;
+            digits.push_back(0);
+        }
+        else
+        {
+            digits[j] += 1;
         }
         return digits;
     }
