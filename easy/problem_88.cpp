ca75d85a8d457c9fbdfdc203f113d786eba96c3d
Tatev <tatev.srapivonyan@gmail.com>
2024-09-14 20:39:02 +0400

    update 88

--- easy/problem_88.cpp
+++ easy/problem_88.cpp
@@ -5,48 +5,6 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        size_t i = 0;
-        size_t j = 0;
-        size_t k = 0;
-        vector<int> arr(m);
-        arr = nums1;
-
-        while (i < m && j < n)
-        {
-            if (arr[i] < nums2[j])
-            {
-                nums1[k] = arr[i];
-                ++i;
-            }
-            else
-            {
-                nums1[k] = nums2[j];
-                ++j;
-            }
-            ++k;
-        }
-
-        while (i < m)
-        {
-            nums1[k] = arr[i];
-            ++i;
-            ++k;
-        }
-
-        while (j < n)
-        {
-            nums1[k] = nums2[j];
-            ++j;
-            ++k;
-        }
-    }
-};
-
-//Version 2
-
-class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m - 1;
         int j = n - 1;
         int k = m + n - 1;
