2f8e1d2d97e05cf3011e18210b066e23305d39f4
Tatev <tatev.srapivonyan@gmail.com>
2024-09-14 20:38:29 +0400

    88 v2

--- easy/problem_88.cpp
+++ easy/problem_88.cpp
@@ -41,3 +41,43 @@ public:
         }
     }
 };
+
+//Version 2
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0)
+        {
+            if (nums1[i] < nums2[j])
+            {
+                nums1[k] = nums2[j];
+                --j;
+            }
+            else
+            {
+                nums1[k] = nums1[i];
+                --i;
+            }
+            --k;
+        }
+
+        while (i > -1)
+        {
+            nums1[k] = nums1[i];
+            --i;
+            --k;
+        }
+
+        while (j > -1)
+        {
+            nums1[k] = nums2[j];
+            --j;
+            --k;
+        }
+    }
+};
