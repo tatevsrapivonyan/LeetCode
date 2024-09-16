d8b8860e62a60360c218621ba505a06fca5a281e
Tatev <tatev.srapivonyan@gmail.com>
2024-09-16 08:50:34 +0400

    Update 283

--- easy/problem_283.cpp
+++ easy/problem_283.cpp
@@ -5,23 +5,20 @@ using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for (size_t i = 0; i < nums.size() - 1; ++i)
+        int j = 0;
+
+        for (size_t i = 0; i < nums.size(); ++i)
         {
-            if (nums[i] == 0)
+            if (nums[i] != 0)
             {
-                int j = i + 1;
-                while (j < nums.size())
-                {
-                    if (nums[j] != 0)
-                    {
-                        int tmp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = tmp;
-                        break;
-                    }
-                    ++j;
-                }
+                nums[j] = nums[i];
+                ++j;
             }
+        }
+
+        for (; j < nums.size(); ++j)
+        {
+            nums[j] = 0;
         }
     }
 };
