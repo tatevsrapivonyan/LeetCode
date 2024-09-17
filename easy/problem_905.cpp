358317eacbb552aaf2f6b1d5521d61e16f3aea91
Tatev <tatev.srapivonyan@gmail.com>
2024-09-17 08:39:31 +0400

    905 Update

--- easy/problem_905.cpp
+++ easy/problem_905.cpp
@@ -5,19 +5,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        for (size_t i = 0; i < nums.size() - 1; ++i)
+        size_t j = 0;
+        for (size_t i = 0; i < nums.size(); ++i)
         {
-            if (nums[i] % 2 != 0)
+            if (nums[i] % 2 == 0)
             {
-                for (int j = i + 1; j < nums.size(); ++j)
-                {
-                    if (nums[j] % 2 == 0)
-                    {
-                        int tmp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = tmp;
-                    }
-                }
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+                ++j;
             }
         }
         return nums;
